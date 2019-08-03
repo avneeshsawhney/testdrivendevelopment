49e33956d950bbd8f51a0703423c5525e8c6c418
Avneesh Sawhney <avneesh.sawhney@pepperdine.edu>
2019-08-03 19:21:47 +0000

    TDD finishing touches

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,59 +1,51 @@
 class GramsController < ApplicationController
-   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
-   def destroy
+  def destroy
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
-    return render_not_found(:forbidden) if @gram.user != current_user
     @gram.destroy
     redirect_to root_path
   end
 
-   def update
+  def update
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
-   return render_not_found(:forbidden) if @gram.user != current_user
+
     @gram.update_attributes(gram_params)
 
     if @gram.valid?
-    redirect_to root_path
-  else
-    return render :edit, status: :unprocessable_entity
+      redirect_to root_path
+    else
+      return render :edit, status: :unprocessable_entity
+    end
   end
 
-  end
-
-
-def new
+  def new
     @gram = Gram.new
   end
-
 
   def index
     @grams = Gram.all
   end
 
   def show
-     @gram = Gram.find_by_id(params[:id])
-      return render_not_found if @gram.blank?
-  end
-  end
-
-   def edit
     @gram = Gram.find_by_id(params[:id])
-  return render_not_found if @gram.blank?
-  return render_not_found(:forbidden) if @gram.user != current_user
- 
+    render_not_found if @gram.blank?
   end
 
+  def edit
+    @gram = Gram.find_by_id(params[:id])
+    render_not_found if @gram.blank?
+  end
 
-   def create
+  def create
     @gram = current_user.grams.create(gram_params)
     if @gram.valid?
-    redirect_to root_path
-  else
-    render :new, status: :unprocessable_entity
-  end
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
@@ -64,3 +56,5 @@ def new
 
 
 
+
+end
